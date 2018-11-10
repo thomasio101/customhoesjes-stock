2e6a6763e81185a1ee07dbcc9ac3251875e02d7b
Thomas Egbert Duursma <thomasduursma@outlook.com>
2018-11-10 18:21:25 +0100

    Optimize MySQL connections
    Made it so that there is only one MySQL connection per thread at any time.
    Also added a garbage collector which cleans up MySQL connections that were in use by dead threads.
    Due to this, all other calls to `connection.close` have been removed.

--- server/main.py
+++ server/main.py
@@ -1,12 +1,56 @@
 import mysql.connector
+import threading
+import time
+from timeit import default_timer as timer
+
+connections = {}
+
+class ConnectionGarbageCollector(threading.Thread):
+    lock = threading.Lock()
+
+    def __init__(self, cycle_length):
+        super().__init__(daemon=True)
+        self.cycle_length = cycle_length
+        self.start()
+
+    def run(self):
+        while True:
+            time.sleep(self.cycle_length)
+            with ConnectionGarbageCollector.lock:
+                dead_thread_ids = []
+                for thread_id in connections:
+                    for thread in threading.enumerate():
+                        alive = thread.ident == thread_id
+                        if alive:
+                            break
+                    if not alive:
+                        dead_thread_ids.append(thread_id)
+
+                for thread_id in dead_thread_ids:
+                    connections[thread_id].close()
+                    del connections[thread_id]
+                if dead_thread_ids:
+                    print("Cleaned up {} connection{}!".format(len(dead_thread_ids), 's' * (len(dead_thread_ids) != 1)))
+
+ConnectionGarbageCollector(10)
 
 def get_db_connection():
-    return mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="customhoesjes_stock"
-    )
+    global connections
+    thread_id = threading.get_ident()
+
+    with ConnectionGarbageCollector.lock:
+        if thread_id in connections and not connections[thread_id].is_connected():
+            connections[thread_id].reconnect()
+        elif thread_id not in connections:
+            connections[thread_id] = mysql.connector.connect(
+            buffered=True,
+            host="localhost",
+            user="root",
+            passwd="",
+            database="customhoesjes_stock"
+            )
+
+        return connections[thread_id]
 
 class ProductMeta(type):
     @property
@@ -22,22 +66,16 @@ class ProductMeta(type):
 
     @property
     def all(self):
-        try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-            cursor.execute("SELECT id FROM products;")
+        cursor.execute("SELECT id FROM products;")
 
+        row = cursor.fetchone()
+
+        while row:
+            yield Product(*row)
             row = cursor.fetchone()
-
-            while row:
-                yield Product(*row)
-                row = cursor.fetchone()
-        finally:
-            try:
-                connection.close()
-            except Exception as e:
-                raise e
 
 class Product(metaclass=ProductMeta):
     def __init__(self, *args):
@@ -50,132 +88,102 @@ class Product(metaclass=ProductMeta):
                 raise TypeError("The provided id has to be an int!")
             else:
                 ## Check if a product exists with the provided id
-                try:
-                    connection = get_db_connection()
-                    cursor = connection.cursor()
+                connection = get_db_connection()
+                cursor = connection.cursor()
 
-                    cursor.execute(
-                    "SELECT 1 FROM products WHERE id=%s;",
-                    (self.id,)
-                    )
+                cursor.execute(
+                "SELECT 1 FROM products WHERE id=%s;",
+                (self.id,)
+                )
 
-                    if not cursor.fetchall():
-                        raise ValueError("There is no product with the provided id!")
-                finally:
-                    try:
-                        connection.close()
-                    except Exception as e:
-                        raise e
+                if not cursor.fetchall():
+                    raise ValueError("There is no product with the provided id!")
         elif arg_count == len(Product.fields):
             try:
                 self.id = int(args[0])
             except ValueError:
                 raise TypeError("The provided id has to be an int!")
             else:
-                try:
-                    connection = get_db_connection()
-                    cursor = connection.cursor()
+                connection = get_db_connection()
+                cursor = connection.cursor()
 
+                cursor.execute(
+                "SELECT 1 FROM products WHERE id=%s;",
+                (self.id,)
+                )
+
+                if cursor.fetchall():
+                    raise ValueError("There is already a product with the provided id!")
+                else:
                     cursor.execute(
-                    "SELECT 1 FROM products WHERE id=%s;",
-                    (self.id,)
+                    "INSERT INTO products({}) VALUES ({});"
+                    .format(",".join(Product.fields), ",".join(['%s'] * len(Product.fields))),
+                    args
                     )
 
-                    if cursor.fetchall():
-                        raise ValueError("There is already a product with the provided id!")
-                    else:
-                        cursor.execute(
-                        "INSERT INTO products({}) VALUES ({});"
-                        .format(",".join(Product.fields), ",".join(['%s'] * len(Product.fields))),
-                        args
-                        )
-
-                        connection.commit()
-                finally:
-                    try:
-                        connection.close()
-                    except Exception as e:
-                        raise e
+                    connection.commit()
         else:
             raise ValueError("expected 1 or {} arguments, got {}".format(len(Product.fields), arg_count))
 
     def delete(self):
-        try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
+        cursor.execute(
+        "SELECT 1 FROM products WHERE id=%s;",
+        (self.id,)
+        )
+
+        if not cursor.fetchall():
+            raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
+        else:
             cursor.execute(
-            "SELECT 1 FROM products WHERE id=%s;",
+            "DELETE FROM products WHERE id = %s;",
             (self.id,)
             )
 
-            if not cursor.fetchall():
-                raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
-            else:
-                cursor.execute(
-                "DELETE FROM products WHERE id = %s;",
-                (self.id,)
-                )
-
-                connection.commit()
-        finally:
-            try:
-                connection.close()
-            except Exception as e:
-                raise e
+            connection.commit()
 
 def get_field_getter(field):
     def field_getter(self):
-        try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-            cursor.execute(
-            "SELECT {} FROM products WHERE id=%s;"
-            .format(field),
-            (self.id,)
-            )
+        cursor.execute(
+        "SELECT {} FROM products WHERE id=%s;"
+        .format(field),
+        (self.id,)
+        )
 
-            results = cursor.fetchall()
+        results = cursor.fetchall()
 
-            if results:
-                return results[0][0]
-            else:
-                raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
-        finally:
-            try:
-                connection.close()
-            except Exception as e:
-                raise e
+        if results:
+            return results[0][0]
+        else:
+            raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
 
     return field_getter
 
 def get_field_setter(field):
     def field_setter(self, value):
-        try:
-            connection = get_db_connection()
-            cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
+        cursor.execute(
+        "SELECT 1 FROM products WHERE id=%s;",
+        (self.id,)
+        )
+
+        if not cursor.fetchall():
+            raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
+        else:
             cursor.execute(
-            "SELECT 1 FROM products WHERE id=%s;",
-            (self.id,)
+            "UPDATE products SET {} = %s WHERE id=%s;"
+            .format(field),
+            (value, self.id)
             )
 
-            if not cursor.fetchall():
-                raise ValueError("There is no product with the provided id!\rAre you trying to access a deleted product?")
-            else:
-                cursor.execute(
-                "UPDATE products SET {} = %s WHERE id=%s;"
-                .format(field),
-                (value, self.id)
-                )
-
-                connection.commit()
-        finally:
-            try:
-                connection.close()
-            except Exception as e:
-                raise e
+            connection.commit()
 
     return field_setter
 
